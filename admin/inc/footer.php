e6e463ccae0ae3985a0432adccf7fd09c2d5be14
sannlwinmoe03 <sannlwinmoe19@gmail.com>
2024-01-06 11:04:19 +0630

    first commit

---
+++ admin/inc/footer.php
@@ -0,0 +1,14 @@
+<footer class="footer">
+    <div class="container-fluid">
+        <nav class="pull-left">
+            <ul>
+
+                
+                
+            </ul>
+        </nav>
+        <div class="copyright pull-right">
+            &copy; <script>document.write(new Date().getFullYear())</script>, made with <i class="fa fa-heart heart"></i> by C Home</a>
+        </div>
+    </div>
+</footer>
---
+++ inc/about.php
@@ -0,0 +1,10 @@
+<div class="about_area" id="about">
+  <div class="about_main container">
+    <?php $h = '1px'; $title='About'; include 'inc/heading_title.php'; ?>
+    <div class="about_para">
+      <p class="text-justify">
+“Online home renting system for C Home” is the system for people who wants to rent house, apartment and compound easily. There are two roles in this system. They are owner and tenant. The owners possess many houses, apartment and compound. Some are possessed the company. In the history, the company advertised with traditional advertisement. Also, the company made renting process from outside traditionally. The company rents owners’ property for citizens but they want to rent not only for citizens but also for foreigner via online. There are four department in company. They are Administrative Department, Finance and HR Department, Operation and Marketing Department and Engineering Department. The Administrative department mange most of the process within office. This Department will manage website admin and users. Most of development processes and goals are depended on this department. Finance and HR Department manage income and outcome and human resource of the company. This department will check and analysis the payments from website and also outside payment. Then they will analyze outcome for employee salary and decoration and maintenance fee for Engineering Department. Operation and Marketing Department will directly manage website. They will market company’s property, find the customers and do customer service. Also, they will advertise company’s products. Engineering Department decorate new home and maintenance existent homes.  Managing properties are done by Administrative Department. 
+</p>
+    </div>
+  </div>
+</div>
